10a56726620924aba1a8bed7fb789d55d7ef46fc
gutosoethe <gutosoethe@gmail.com>
2022-05-28 14:40:03 -0300

    problema 9

---
+++ src/beecrowd/Main.java
@@ -0,0 +1,25 @@
+package beecrowd;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+    	String NAME;
+    	double FIXED_SALARY, TOTAL_SALES, SALARY;
+    	
+    	Scanner s = new Scanner(System.in);
+    	
+    	NAME = s.next();
+    	FIXED_SALARY = s.nextDouble();
+    	TOTAL_SALES = s.nextDouble();
+    	
+    	SALARY = FIXED_SALARY+(TOTAL_SALES*0.15);
+    	
+    	System.out.printf("TOTAL = R$ %.2f%n",SALARY);
+ 
+    }
+ 
+}
